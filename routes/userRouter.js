d0f15684157bcdb902be3e64efbf61f6c3d2e43b
Luis E Rondon <leduardo.rondon@gmail.com>
2022-06-05 00:21:59 -0400

    change auth post user

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -5,7 +5,7 @@ import { auth } from '../middleware/auth.js';
 const userRouter = express.Router();
 userRouter.get('/', auth, getAll);
 userRouter.get('/:id', auth, getById);
-userRouter.post('/', auth, post);
+userRouter.post('/', post);
 userRouter.put('/:id', auth, put);
 userRouter.post('/login', login);
 userRouter.delete('/:id', auth, del);
