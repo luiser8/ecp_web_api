d12a971ab02a0af2ef1b5360009087286f99128e
Luis E Rondon <leduardo.rondon@gmail.com>
2022-06-05 18:42:18 -0400

    env changes for production

--- config/database.js
+++ config/database.js
@@ -2,8 +2,8 @@ import mongoose  from 'mongoose';
 import dotenv from 'dotenv';
 
 dotenv.config();
-
-    let database = await mongoose.connect(process.env.DB, {
+    const env = process.env.NODE_ENV === "development" ? process.env.MONGO_URI_LOCAL : process.env.MONGO_URI;
+    const database = await mongoose.connect(env, {
         useNewUrlParser: true, 
         useUnifiedTopology: true
     }).then().catch(err => console.error(err));
