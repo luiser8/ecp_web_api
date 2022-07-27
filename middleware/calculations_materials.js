ae3ac773171f1ea9039fdab1431671805e471e33
Luis Rondon <leduardo.rondon@gmail.com>
2022-07-27 14:10:17 -0400

    Validacion si llega un valor cero a los calculos

--- middleware/calculations_materials.js
+++ middleware/calculations_materials.js
@@ -10,7 +10,7 @@ export const calculations_materials = (...values) => {
 
     materials.map((_, item) => {
         const calc = materials[item];
-        calc.cost_unit_x_mix = calc.cost_x_mix / calc.qty_x_mix;
+        calc.cost_unit_x_mix = !calc.qty_x_mix === 0 ? calc.cost_x_mix / calc.qty_x_mix : calc.cost_x_mix;
         calc.qty_x_box = calc.qty_x_mix / boxes_x_mix;
         calc.cost_x_box = calc.cost_x_mix / boxes_x_mix;
         calc.qty_x_unit = calc.qty_x_mix / units_x_mix;
